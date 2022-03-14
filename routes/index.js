af8a4f47730c99da1456113b5b83185306106e2d
Don Schenck <don.schenck@gmail.com>
2022-03-14 11:06:12 -0400

    Added quotes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'QuoteSite' });
+  res.render('index', { title: "QuoteSite" });
 });
 
 module.exports = router;
